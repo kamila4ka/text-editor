e2e59258f7f08d9f08243f46b976941850235325
kamila4ka <48149585+kamila4ka@users.noreply.github.com>
2019-06-16 13:06:28 +0300

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,47 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+//#include "ui_mainwindow.h"
+#include <QMainWindow>
+#include "filemanager.h"
+#include "appearance.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void readFile();
+
+    void saveFileAs();
+
+    void saveFile();
+
+    void pickFont();
+
+    void changeTextColor();
+
+    void changeBackground();
+
+    void changeTextBackground();
+
+    void createNewFile();
+
+private:
+    Ui::MainWindow *ui;
+
+    QString filepath;
+
+    FileManager *pFiles;
+
+    Appearance *pAppear;
+};
+
+#endif // MAINWINDOW_H
