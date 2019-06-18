42d3fef296df596a15aa3b575f441b6920d26207
kamila4ka <48149585+kamila4ka@users.noreply.github.com>
2019-06-18 22:02:49 +0300

    Update appearance.cpp

--- appearance.cpp
+++ appearance.cpp
@@ -4,23 +4,32 @@
 #include <QFont>
 #include <QPalette>
 
-Appearance::Appearance(QWidget *parent) : QMainWindow(parent) {
+Appearance::Appearance(QWidget *parent) : QMainWindow(parent) 
+{
 }
 
-void Appearance::changeTextColor(QColor color, QTextEdit* txt) {
+void Appearance::changeTextColor(QColor color, 
+                                 QTextEdit* txt) 
+{
     txt->setTextColor(color);
 }
 
-void Appearance::changeBackgroundColor(QColor color, QTextEdit* txt) {
+void Appearance::changeBackgroundColor(QColor color, 
+                                       QTextEdit* txt) 
+{
     QPalette p(palette());
     p.setColor(QPalette::Base, color);
     txt->setPalette(p);
 }
 
-void Appearance::pickFont(QFont font, QTextEdit* txt) {
+void Appearance::pickFont(QFont font, 
+                          QTextEdit* txt) 
+{
     txt->setCurrentFont(font);
 }
 
-void Appearance::changeTextBackground(QColor color, QTextEdit* txt) {
+void Appearance::changeTextBackground(QColor color, 
+                                      QTextEdit* txt) 
+{
     txt->setTextBackgroundColor(color);
 }
